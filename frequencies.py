7fce04ed909d6bda70b8e90b4e2e7e4934a93600
Hui Shing Chong <hui.chong@kcl.ac.uk>
2022-09-28 12:31:20 +0100

    Added my solution for frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,16 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        if not isinstance(item, str):
+            item = str(item)
+        if item not in frequencies:
+            frequencies[f'{item}'] = 1
+        else:
+            frequencies[f'{item}'] += 1
     return frequencies
+
+print(frequencies(['a', 'a', 'b', 'b', 'b', 'c']))
+#{ 'a': 2, 'b': 3, 'c': 1 }
+print(frequencies([100, 'Hello', '100', '100', 100]))
+#{ '100': 4, 'Hello': 1 }
